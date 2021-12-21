054f97de3f133c94ef818f907b81929803c648f3
m-triple-m <triplem656@gmail.com>
2021-12-21 19:27:50 +0530

    ..

--- Back-end/connection.js
+++ Back-end/connection.js
@@ -1,10 +1,15 @@
-const mongoose =require("mongoose");
+const mongoose = require("mongoose");
 
-
-const uri="mongodb+srv://nasimkhan:nasimkhan@cluster0.tpjhm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+const uri =
+  "mongodb+srv://nasimkhan:nasimkhan@cluster0.tpjhm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 //promise
-mongoose.connect(uri)
-.then(() =>{console.log('database connected')})
-.catch ((err) => {console.error(err)});
+mongoose
+  .connect(uri)
+  .then(() => {
+    console.log("database connected");
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 
-module.exports= mongoose;
+module.exports = mongoose;
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,19 @@
 import { createTheme, ThemeProvider } from "@mui/material";
 import { dark } from "@mui/material/styles/createPalette";
 import { useState } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Addpaper from "./components/addpaper";
 import Header from "./components/header";
 import Home from "./components/home";
-import AboutUs  from "./components/about";
+import AboutUs from "./components/about";
 import Course from "./components/course";
 import Login from "./components/login";
 import SignUp from "./components/signup";
 import { ProdductProvider } from "./productContext";
 import Dashboard from "./components/dashboard";
 import Branch from "./components/branch";
+import SolvePaper from "./components/solvepaper";
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
@@ -20,44 +21,40 @@ function App() {
   const lightTheme = createTheme({
     palette: {
       mode: "light",
-      primary:{
-        main:"#e6244ed7",
-        dark:"red",
+      primary: {
+        main: "#e6244ed7",
+        dark: "red",
       },
     },
-
-
   });
-const darkTheme = createTheme({
+  const darkTheme = createTheme({
     palette: {
-      mode:  "dark" ,
-     background:{
-       paper:"#1c113a",
-     }
+      mode: "dark",
+      background: {
+        paper: "#1c113a",
+      },
     },
-
   });
 
   return (
-    <ThemeProvider theme={darkMode ? darkTheme: lightTheme}>
+    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
       <ProdductProvider>
-      <BrowserRouter>
-        <Header darkMode={darkMode} setDarkMode={setDarkMode}></Header>
-      <Routes>
+        <BrowserRouter>
+          <Header darkMode={darkMode} setDarkMode={setDarkMode}></Header>
+          <Routes>
+            <Route path="/home" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/addpaper" element={<Addpaper />} />
+            <Route path="/aboutus" element={<AboutUs />} />
+            <Route path="/Course" element={<Course />} />
+            <Route path="/Branch" element={<Branch />} />
 
-        <Route path="/home" element={<Home/>} />
-        <Route path="/login" element={<Login/>} />
-        <Route path="/signup" element={<SignUp/>} />
-        <Route path="/addpaper" element={<Addpaper/>} />
-        <Route path="/aboutus" element={<AboutUs/>} />
-        <Route path="/Course" element={<Course/>} />
-        <Route path="/Branch" element={<Branch/>} />
-
-        <Route path="/dashboard" element={<Dashboard/>} />
-
-       
-      </Routes>
-      </BrowserRouter>
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/" element={<Navigate replace to="/home" />} />
+            <Route component={<SolvePaper />} path={"/solve/:id"}></Route>
+          </Routes>
+        </BrowserRouter>
       </ProdductProvider>
     </ThemeProvider>
   );
